292b174c0d537d0094be7ac991ec06a689e504cf
seragsabrysakr <srag.sabry@gmail.com>
2022-08-25 19:58:50 -0700

    initial commit

---
+++ lib/pick_screen.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PickScreen extends StatefulWidget {
+  const PickScreen({Key? key}) : super(key: key);
+  static const String id = 'PromoScreen';
+
+  @override
+  State<PickScreen> createState() => _PickScreenState();
+}
+
+class _PickScreenState extends State<PickScreen> {
+  Color primColor = const Color(0Xff2D364C);
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    if (Platform.isIOS) WebView.platform = CupertinoWebView();
+  }
+
+  bool isLoading = true;
+  final _key = UniqueKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        WebView(
+          key: _key,
+          initialUrl: 'https://delivery.pick-a.net',
+          javascriptMode: JavascriptMode.unrestricted,
+          onProgress: (int progress) {
+            print('WebView is loading (progress : $progress%)');
+          },
+          onPageFinished: (finish) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+        ),
+        isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Stack(),
+      ],
+    ));
+  }
+}
