9d348aa59d42b6d4ab06ca94b009d82a0327b343
aatia27@gmail.com <kPYWrD58LNXDMHg>
2022-10-17 22:43:23 +0200

    adding JavascriptChannel and handle back button

--- lib/pick_screen.dart
+++ lib/pick_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PickScreen extends StatefulWidget {
   const PickScreen({Key? key}) : super(key: key);
@@ -12,44 +14,74 @@ class PickScreen extends StatefulWidget {
 }
 
 class _PickScreenState extends State<PickScreen> {
-  Color primColor = const Color(0Xff2D364C);
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-    if (Platform.isIOS) WebView.platform = CupertinoWebView();
+  WebViewController? _controller;
+
+  final Completer<WebViewController> _controllerCompleter =
+  Completer<WebViewController>();
+
+  //Make sure this function return Future<bool> otherwise you will get an error
+  Future<bool> _onWillPop(BuildContext context) async {
+    if (await _controller!.canGoBack()) {
+      _controller?.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
+  final _key = UniqueKey();
 
   bool isLoading = true;
-  final _key = UniqueKey();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xffff7800),
-      child: SafeArea(
-        child: Scaffold(
-            body: Stack(
-          children: [
-            WebView(
-              key: _key,
-              initialUrl: 'https://delivery.pick-a.net',
-              javascriptMode: JavascriptMode.unrestricted,
-              onProgress: (int progress) {
-                print('WebView is loading (progress : $progress%)');
-              },
-              onPageFinished: (finish) {
-                setState(() {
-                  isLoading = false;
-                });
-              },
-            ),
-            isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Stack(),
-          ],
-        )),
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        body: SafeArea(
+            child: Stack(
+              children: [
+                WebView(
+                  initialUrl: 'https://delivery.pick-a.net',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  key: _key,
+                  onWebViewCreated: (WebViewController webViewController) {
+                    _controllerCompleter.future
+                        .then((value) => _controller = value);
+                    _controllerCompleter.complete(webViewController);
+                  },
+                  javascriptChannels: {
+                    JavascriptChannel(
+                        name: 'opengooglemaps',
+                        onMessageReceived: (JavascriptMessage message) async {
+                          if (!await launchUrl(Uri.parse(message.message),
+                              mode: LaunchMode.externalApplication)) {
+                            throw 'Could not launch $message';
+                          }
+                        })
+                  },
+                  onProgress: (int progress) {
+                    print('WebView is loading (progress : $progress%)');
+                  },
+                  onPageFinished: (finish) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                  },
+                ),
+                isLoading
+                    ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : Stack(),
+              ],
+            )
+          // WebView(
+
+          //   javascriptMode: JavascriptMode.unrestricted,
+          //   initialUrl: widget.url,
+          // )
+
+        ),
       ),
     );
   }
