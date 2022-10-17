9c8a5aa53605a2b637950182ae6675b2002ffbb3
seragsabrysakr <srag.sabry@gmail.com>
2022-10-17 21:30:09 +0200

    update

--- lib/pick_screen.dart
+++ lib/pick_screen.dart
@@ -24,28 +24,33 @@ class _PickScreenState extends State<PickScreen> {
   final _key = UniqueKey();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        WebView(
-          key: _key,
-          initialUrl: 'https://delivery.pick-a.net',
-          javascriptMode: JavascriptMode.unrestricted,
-          onProgress: (int progress) {
-            print('WebView is loading (progress : $progress%)');
-          },
-          onPageFinished: (finish) {
-            setState(() {
-              isLoading = false;
-            });
-          },
-        ),
-        isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : Stack(),
-      ],
-    ));
+    return Container(
+      color: Color(0xffff7800),
+      child: SafeArea(
+        child: Scaffold(
+            body: Stack(
+          children: [
+            WebView(
+              key: _key,
+              initialUrl: 'https://delivery.pick-a.net',
+              javascriptMode: JavascriptMode.unrestricted,
+              onProgress: (int progress) {
+                print('WebView is loading (progress : $progress%)');
+              },
+              onPageFinished: (finish) {
+                setState(() {
+                  isLoading = false;
+                });
+              },
+            ),
+            isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Stack(),
+          ],
+        )),
+      ),
+    );
   }
 }
